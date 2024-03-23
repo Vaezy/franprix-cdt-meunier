452eeeafdb42f29dc1fedfd88268d07783130eb6
Vaezy <boy1.thoomas@gmail.com>
2024-03-23 01:28:18 +0100

    feat: 🎸 add product container

--- js/script.js
+++ js/script.js
@@ -1,16 +1,13 @@
 const searchInput = document.getElementById('searchInput');
 const clearSearch = document.getElementById('clearSearch');
 
-// Fonction pour afficher ou masquer les boutons
 function toggleButtons() {
     clearSearch.style.display = searchInput.value ? 'block' : 'none';
 }
 
-// Ajouter un événement de saisie à l'input
 searchInput.addEventListener('input', toggleButtons);
 
-// Ajouter un événement de clic au bouton de suppression
 clearSearch.addEventListener('click', () => {
-    searchInput.value = ''; // Efface le contenu de l'input
-    toggleButtons(); // Masque le bouton de suppression
+    searchInput.value = '';
+    toggleButtons();
 });
